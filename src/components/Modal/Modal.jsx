e37c4b51b3506ca8c37153995ef6aa94be929413
Nikolay Naumenko <nnv2006@ukr.net>
2023-11-13 14:30:30 +0200

    UPD: Modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,64 +1,32 @@
 import { AiOutlineCloseCircle } from 'react-icons/ai';
 
-import { Box, IconButton, useTheme } from '@mui/material';
 import Modal from '@mui/material/Modal';
 
 import PropTypes from 'prop-types';
 
-export default function ModalWindow({ handleClose, children, open }) {
-  const theme = useTheme();
+import { BoxStyled, IconButtonStyled } from './Modal.styled';
 
-  const style = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    minWidth: 200,
-    maxWidth: 700,
-
-    bgcolor: 'background.paper',
-    border: `1px solid ${theme.palette.primary.main}`,
-    borderRadius: 5,
-    boxShadow: 24,
-    p: 4,
-  };
-
-  const iconButtonStyle = {
-    position: 'absolute',
-    top: 10,
-    right: 10,
-    padding: 0,
-    backgroundColor: 'transparent',
-    color: 'primary',
-    borderRadius: '50%',
-    transition: 'color 250ms linear, transform 250ms linear',
-    '&:hover': {
-      color: theme.palette.primary.main,
-      transform: 'rotate(180deg)',
-    },
-  };
-
+export default function AppWindow({ handleClose, children, open }) {
   return (
     <Modal keepMounted open={open} onClose={handleClose}>
-      <Box sx={style}>
-        <IconButton
+      <BoxStyled>
+        <IconButtonStyled
           aria-label="delete"
           color="secondary"
           onClick={handleClose}
-          size="small"
-          sx={iconButtonStyle}
+          size="large"
         >
-          <AiOutlineCloseCircle fontSize="40" />
-        </IconButton>
+          <AiOutlineCloseCircle fontSize="50" />
+        </IconButtonStyled>
         {children}
-      </Box>
+      </BoxStyled>
     </Modal>
   );
 }
 
-ModalWindow.propTypes = {
+AppWindow.propTypes = {
   handleOpen: PropTypes.func.isRequired,
   handleClose: PropTypes.func.isRequired,
   open: PropTypes.bool.isRequired,
-  children: PropTypes.node,
+  children: PropTypes.isRequired,
 };

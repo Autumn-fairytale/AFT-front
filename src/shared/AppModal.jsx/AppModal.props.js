21f2583b701be58ca9cecfa224f6486caf2a02be
Nikolay Naumenko <nnv2006@ukr.net>
2023-11-13 14:57:33 +0200

    UPD: Modal props

--- src/shared/AppModal.jsx/AppModal.props.js
+++ src/shared/AppModal.jsx/AppModal.props.js
@@ -3,5 +3,5 @@ import PropTypes from 'prop-types';
 export const AppModalProps = {
   handleClose: PropTypes.func.isRequired,
   open: PropTypes.bool.isRequired,
-  children: PropTypes.isRequired,
+  children: PropTypes.node.isRequired,
 };

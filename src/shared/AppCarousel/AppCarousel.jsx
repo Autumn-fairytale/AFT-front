4018a6915791b2ee273aedb4b32f65ff42655c4a
Mykola <108565397+NNaumenko83@users.noreply.github.com>
2023-11-20 00:20:39 +0200

    ADD: carousel

--- src/shared/AppCarousel/AppCarousel.jsx
+++ src/shared/AppCarousel/AppCarousel.jsx
@@ -1,32 +1,93 @@
+import { Component } from 'react';
 import Slider from 'react-slick';
 
-import DishCard from '@/components/DishCard/DishCard';
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
 
-const settings = {
-  dots: true,
-  infinite: true,
-  speed: 500,
-  slidesToShow: 3,
-  slidesToScroll: 3,
-};
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
-export const AppCarousel = () => {
+export class AppCarousel extends Component {
+  render() {
+    var settings = {
+      dots: true,
+      infinite: true,
+      speed: 1500,
+      slidesToShow: 5,
+      slidesToScroll: 5,
+      initialSlide: 0,
+      autoplaySpeed: 10000,
+
+      autoplay: true,
+
+      responsive: [
+        {
+          breakpoint: 1200,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true,
+          },
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            initialSlide: 2,
+          },
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            dots: false,
+          },
+        },
+      ],
+    };
+    return (
+      <div
+        style={{
+          padding: '50px',
+        }}
+      >
+        <Slider {...settings}>
+          <MediaCard />
+          <MediaCard />
+          <MediaCard />
+          <MediaCard />
+          <MediaCard />
+          <MediaCard />
+          <MediaCard />
+          <MediaCard />
+        </Slider>
+      </div>
+    );
+  }
+}
+
+export function MediaCard() {
   return (
-    <div>
-      <h2> Multiple items </h2>
-      <Slider {...settings}>
-        <DishCard />
-        <DishCard />
-        <DishCard />
-        <DishCard />
-        <DishCard />
-        <DishCard />
-        <DishCard />
-        <DishCard />
-        <DishCard />
-        <DishCard />
-        <DishCard />
-      </Slider>
-    </div>
+    <Card sx={{ maxWidth: 220 }}>
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Lizard
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Share</Button>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
   );
-};
+}

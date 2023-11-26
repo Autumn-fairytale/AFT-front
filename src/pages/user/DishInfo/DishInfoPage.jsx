2af7eb1fa5cd76aaf3f0fab5dcef8974a1040f5d
Mykola <108565397+NNaumenko83@users.noreply.github.com>
2023-11-26 20:47:35 +0200

    ADD: reviewsList to Dishes page

--- src/pages/user/DishInfo/DishInfoPage.jsx
+++ src/pages/user/DishInfo/DishInfoPage.jsx
@@ -1,10 +1,16 @@
 import { useParams } from 'react-router-dom';
 
+import { ReviewsList } from '@/components/ReviewsList/ReviewsList';
 import { Main } from '@/shared/Main/Main';
 
 const DishInfoPage = () => {
   const { dishId } = useParams();
-  return <Main>Dish Info Page: {dishId}</Main>;
+  return (
+    <Main>
+      Dish Info Page: {dishId}
+      <ReviewsList id={dishId} />
+    </Main>
+  );
 };
 
 export default DishInfoPage;

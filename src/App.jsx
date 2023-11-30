bc13489b37a832d3e7c7a3bc0014ffc2b5e493cd
kyzym <kyzym1@gmail.com>
2023-11-30 12:34:50 +0100

    refactor: chefsAmountAfterFee

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,12 @@
-// import AppRouter from './routes/AppRouter';
-
-// import { AddDishForm } from './components/AddDishForm/AddDishForm';
-
-import { Container, Stack } from '@mui/material';
-
-import { ChefOrdersTable } from './components/ChefOrdersTable/ChefOrdersTable';
-import { UserOrdersTable } from './components/UserOrdersTable/UserOrdersTable';
+import Header from './components/Header/Header';
+import AppRouter from './routes/AppRouter';
 
 function App() {
   return (
-    <Container maxWidth="lg">
-      <Stack direction={'column'} spacing={10}>
-        <ChefOrdersTable />
-
-        <UserOrdersTable />
-      </Stack>
-      {/* <AppRouter /> */}
-    </Container>
+    <>
+      <Header />
+      <AppRouter />
+    </>
   );
 }
 

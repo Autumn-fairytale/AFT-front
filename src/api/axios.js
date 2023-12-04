d6ba17cd4561a3c36bc35b3257cbcf6ce1100f0e
Andrii Zaimak <dev.andrii.zaimak@gmail.com>
2023-12-04 15:30:04 +0200

    refactor: remove mock data

--- src/api/axios.js
+++ src/api/axios.js
@@ -4,11 +4,6 @@ const { VITE_API_URL } = import.meta.env;
 
 export const privateInstance = axios.create({
   baseURL: VITE_API_URL,
-  // TODO: Delete mock data
-  headers: {
-    Authorization:
-      'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1NjQ0NTc0YzY2OGEzMzQ2OGYwMDkwOSIsImlhdCI6MTcwMTA3MDg1MX0.xrFoszfqHDv_amb3Ec6kDvjsXztxkr7RpPJczn_x6Ss',
-  },
 });
 
 export const publicInstance = axios.create({

4948098c9a7180b7f89b971916737807c90b281e
Nikolay Naumenko <nnv2006@ukr.net>
2023-12-05 15:07:09 +0200

    UPD: DishesFilter

--- src/components/DishesFilter/DishesFilter.jsx
+++ src/components/DishesFilter/DishesFilter.jsx
@@ -1,15 +1,70 @@
-// import { CATEGORIES, CUISINES } from '@/constants';
-import CheckboxesTags from '../CheckboxesTags/CheckboxesTags';
+import { useMemo } from 'react';
+import { useSearchParams } from 'react-router-dom';
+
+import Checkbox from '@mui/material/Checkbox';
+import FormControlLabel from '@mui/material/FormControlLabel';
+
+import { CATEGORIES, CUISINES } from '@/constants';
+import { AppSelect } from '@/shared';
+import { useTheme } from '@emotion/react';
 
 export const DishesFilter = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  console.log('searchParams:', searchParams);
+
+  const theme = useTheme();
+
+  const params = useMemo(
+    () => Object.fromEntries([...searchParams]),
+    [searchParams]
+  );
+
+  const handleInputChange = (e) => {
+    setSearchParams({ ...params, [e.target.name]: e.target.value });
+  };
+
+  const handleCheckboxChange = (e) => {
+    setSearchParams({
+      ...params,
+      [e.target.name]: !JSON.parse(params?.isVegan || 'false'),
+    });
+  };
+
   return (
     <div>
       <h5>DishesFilter</h5>
-      <CheckboxesTags />
-      <CheckboxesTags />
-      <CheckboxesTags />
-      <CheckboxesTags />
-      <CheckboxesTags />
+      <div style={{ display: 'flex', gap: '15px' }}>
+        <AppSelect
+          options={CATEGORIES}
+          label={'Category'}
+          name="categories"
+          onChange={handleInputChange}
+          value={params.categories || ''}
+        />
+        <AppSelect
+          options={CUISINES}
+          label={'Cuisine'}
+          name="cuisines"
+          onChange={handleInputChange}
+          value={params.cuisines || ''}
+        />
+        <FormControlLabel
+          control={
+            <Checkbox
+              checked={JSON.parse(params?.isVegan || false)}
+              onChange={handleCheckboxChange}
+              name="isVegan"
+              sx={{
+                color: theme.palette.primary.main,
+                '&.Mui-checked': {
+                  color: theme.palette.primary.main,
+                },
+              }}
+            />
+          }
+          label="Vegan"
+        />
+      </div>
     </div>
   );
 };

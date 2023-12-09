a77e026ee80c43d592abdf2a2c36623249fd2954
Mykola <108565397+NNaumenko83@users.noreply.github.com>
2023-12-10 00:47:46 +0200

    UPD: ReviewsForm

--- src/components/ReviewForm/ReviewForm.jsx
+++ src/components/ReviewForm/ReviewForm.jsx
@@ -90,12 +90,12 @@ export const ReviewForm = ({ existingReview, dishId, onClose }) => {
     if (existingReview) {
       await editReviewMutate.mutate({
         rating,
-        review,
+        review: review.trim(),
         dishId,
         reviewId: existingReview.id,
       });
     } else {
-      await addReviewMutate.mutate({ rating, review, dishId });
+      await addReviewMutate.mutate({ rating, review: review.trim(), dishId });
     }
 
     if (!existingReview) {
@@ -151,7 +151,7 @@ export const ReviewForm = ({ existingReview, dishId, onClose }) => {
           variant="contained"
           label="Send"
           type="submit"
-          disabled={!review.length || review.length > 400 || !rating}
+          disabled={!review.trim().length || review.length > 400 || !rating}
         />
       </ButtonWrapper>
     </Form>

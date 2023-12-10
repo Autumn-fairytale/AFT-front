3b65b704a0ad7bb2fca007445dca6587fd68f175
kyzym <kyzym1@gmail.com>
2023-12-10 09:07:17 +0100

    feat: add mock files

---
+++ src/api/fetchNotifications.js
@@ -0,0 +1,7 @@
+// import { privateInstance } from './axios';
+
+// export const fetchNotifications = async () => {
+//   const { data } = await privateInstance.get(`/notifications`);
+
+//   return data;
+// };
---
+++ src/hooks/useNotificationsQuery .js
@@ -0,0 +1,7 @@
+// import { useQuery } from '@tanstack/react-query';
+
+// export const useNotificationsQuery = () => {
+//   return useQuery('notifications', fetchNotifications, {
+//     refetchInterval: 3 * 60000,
+//   });
+// };

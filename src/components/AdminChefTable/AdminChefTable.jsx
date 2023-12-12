6fe6fcb55a1502d091d33cafc4edcb2a3abc396b
Nikolay Naumenko <nnv2006@ukr.net>
2023-12-12 09:44:15 +0200

    ADD: data for table

--- src/components/AdminChefTable/AdminChefTable.jsx
+++ src/components/AdminChefTable/AdminChefTable.jsx
@@ -1,32 +1,65 @@
+import { useMemo } from 'react';
+
+import { Avatar } from '@mui/material';
 import { DataGrid } from '@mui/x-data-grid';
+
+import { format } from 'date-fns';
 
 import { getChefs } from '@/api/chef/getChefs';
 import { useQuery } from '@tanstack/react-query';
 
-function getFullName(params) {
-  console.log('params:', params);
-  return `${params.row.userId.firstName || ''} ${
-    params.row.userId.lastName || ''
-  }`;
+function getFullName({ row: { userId } }) {
+  return `${userId.firstName || ''} ${userId.lastName || ''}`;
 }
 
-const columns = [
-  {
-    field: 'fullName',
-    headerName: 'Full name',
-    width: 160,
-    valueGetter: getFullName,
-  },
-  {
-    field: 'fullName',
-    headerName: 'Full name',
-    width: 160,
-    valueGetter: getFullName,
-  },
-];
-console.log('columns:', columns);
-
 export const AdminChefTable = () => {
+  const columns = useMemo(
+    () => [
+      {
+        field: 'photoURL',
+        headerName: 'Avatar',
+        width: 60,
+        valueGetter: ({ row: { avatar } }) => avatar,
+        renderCell: ({ row: { avatar } }) => <Avatar src={avatar} />,
+        sortable: false,
+        filterable: false,
+      },
+      {
+        field: 'fullName',
+        headerName: 'Full name',
+        width: 170,
+        valueGetter: getFullName,
+      },
+      {
+        field: 'phoneNumber',
+        headerName: 'Phone number',
+        width: 200,
+        valueGetter: ({ row: { phoneNumber } }) => phoneNumber,
+      },
+      {
+        field: 'status',
+        headerName: 'Status',
+        width: 100,
+        valueGetter: ({ row: { isAvailable } }) => isAvailable,
+      },
+      {
+        field: 'createdAt',
+        headerName: 'Created At',
+        width: 200,
+        valueGetter: ({ row: { createdAt } }) =>
+          format(new Date(createdAt), 'dd/MM/yyyy HH:MM:SS'),
+      },
+      {
+        field: '_id',
+        headerName: 'Id',
+        width: 220,
+        valueGetter: ({ row }) => row.id,
+        sortable: false,
+        filterable: false,
+      },
+    ],
+    []
+  );
   const { data } = useQuery({
     queryKey: ['chefs', 'admin'],
     queryFn: getChefs,
@@ -38,7 +71,9 @@ export const AdminChefTable = () => {
   return (
     <div>
       AdminChefTable
-      {data && <DataGrid rows={rows} columns={columns} />}
+      {data && (
+        <DataGrid rows={rows} columns={columns} getRowId={(row) => row.id} />
+      )}
     </div>
   );
 };
